265fceb1d70f6f7fe1a283e9fbbc3e61923847e0
Guanzhou03 <guanzhou03@gmail.com>
2022-06-22 21:31:34 +0800

    Chats page

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,210 +1,187 @@
+import 'dart:io';
+import 'package:close_contact/models/constants.dart';
+import 'package:close_contact/models/database.dart';
+import 'package:close_contact/models/text.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:close_contact/models/message_model.dart';
-import 'package:user_profile_avatar/user_profile_avatar.dart';
 
-import '../firestore/info-getter.dart';
-//import 'package:close_contact/models/user_model.dart';
 
-class ChattingPage extends StatefulWidget {
-  String receiverUID;
-  String senderUID;
-  ChattingPage(this.receiverUID, this.senderUID, {Key? key}) : super(key: key);
+
+class Chat extends StatefulWidget {
+  final String chatRoomId;
+
+  Chat({required this.chatRoomId});
 
   @override
-  _ChatsState createState() => _ChatsState(this.senderUID, this.receiverUID);
+  _ChatState createState() => _ChatState();
 }
 
-class _ChatsState extends State<ChattingPage> {
-  String receiverUID;
-  String senderUID;
-  _ChatsState(this.receiverUID, this.senderUID);
+class _ChatState extends State<Chat> {
+  Stream<QuerySnapshot> chats = Stream.empty();
+  TextEditingController messageEditingController = new TextEditingController();
 
-  List<Message> messages = [];
-  var num = 3;
-  void initialMessages() {
-    for (int i = 0; i < 5; i++) {
-      messages.add(Message(
-        senderID: senderUID,
-        time: DateTime.now().toString(),
-        text: "Test message",
-        unread: true
-      ));
+
+  Widget chatMessages(){
+    return StreamBuilder<QuerySnapshot>(
+      stream: chats,
+      builder: (context, snapshot){
+        return snapshot.hasData ?  ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index){
+              var temp = snapshot.data!.docs[index].data()! as Map;
+              return MessageTile(
+                message: temp["message"],
+                sendByMe: Constants.myName == snapshot.data!.docs[index].data()!,
+              );
+            }) : Container();
+      },
+    );
+  }
+
+  addMessage() {
+    if (messageEditingController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "sendBy": Constants.myName,
+        "message": messageEditingController.text,
+        'time': DateTime
+            .now()
+            .millisecondsSinceEpoch,
+      };
+
+      DatabaseMethods().addMessage(widget.chatRoomId, chatMessageMap);
+
+      setState(() {
+        messageEditingController.text = "";
+      });
     }
   }
-  maptoNames(id) async {
-    String name = await InfoGetter.nameGetter(userID: id);
-    return name;
-  }
-  maptoImage(id) async {
-    String url = await InfoGetter.imageURLGetter(userID:id);
-    return url;
-  }
-  
-  _buildMessage(Message message, bool isMe) {
-    final container = Container(
-      margin: isMe
-          ? EdgeInsets.only(
-        top: 8.0,
-        bottom: 8.0,
-        left: 80.0,
-      )
-          : EdgeInsets.only(
-        top: 8.0,
-        bottom: 8.0,
-      ),
-      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
-      width: MediaQuery.of(context).size.width * 0.75,
-      decoration: BoxDecoration(
-        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
-        borderRadius: isMe
-            ? BorderRadius.only(
-          topLeft: Radius.circular(15.0),
-          bottomLeft: Radius.circular(15.0),
-        )
-            : BorderRadius.only(
-          topRight: Radius.circular(15.0),
-          bottomRight: Radius.circular(15.0),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            message.time,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            message.text,
-            style: TextStyle(
-              color: Colors.black54,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ],
-      ),
-    );
 
-    if (isMe) {
-      return container;
-    }
-
-    return Row(
-      children: <Widget>[
-        container,
-      ],
-    );
-  }
-
-  _buildMessageComposer() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
-      height: 70.0,
-      color: Colors.white,
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.attach_file),
-            iconSize: 25.0,
-            color: Theme.of(context).primaryColor,
-            onPressed: () {},
-          ),
-          Expanded(
-            child: TextField(
-              textCapitalization: TextCapitalization.sentences,
-              onChanged: (value) {},
-              decoration: InputDecoration.collapsed(
-                hintText: 'Send a message...',
-              ),
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.send),
-            iconSize: 25.0,
-            color: Theme.of(context).primaryColor,
-            onPressed: () {},
-          ),
-        ],
-      ),
-    );
+  @override
+  void initState() {
+    DatabaseMethods().getChats(widget.chatRoomId).then((val) {
+      setState(() {
+        chats = val;
+      });
+    });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      appBar: AppBar(
-        leading: UserProfileAvatar(
-          avatarUrl: maptoImage(senderUID),
-          onAvatarTap: () {
-            print("tapped");
-          },
-          avatarSplashColor: Colors.purple,
-          radius: 50,
-          isActivityIndicatorSmall: false,
-          avatarBorderData: AvatarBorderData(
-            borderColor: Colors.white,
-            borderWidth: 5.0,
-          ),
-        ),
-        title: Text(
-          maptoNames(senderUID), //should be the sender user
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        elevation: 0.0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.more_horiz),
-            iconSize: 30.0,
-            color: Colors.white,
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: Column(
-          children: <Widget>[
-            Expanded(
+      appBar: AppBar(title: Text("Chat Room")),
+      body: Container(
+        child: Stack(
+          children: [
+            chatMessages(),
+            Container(alignment: Alignment.bottomCenter,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
               child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                  ),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                  ),
-                  child: ListView.builder(
-                    reverse: true,
-                    padding: EdgeInsets.only(top: 15.0),
-                    itemCount: messages.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      final Message message = messages[index];
-                      final bool isMe = message.senderID == currentUser.id;
-                      return _buildMessage(message, isMe);
-                    },
-                  ),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+                color: Color(0x54FFFFFF),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                          controller: messageEditingController,
+                          style: simpleTextStyle(),
+                          decoration: InputDecoration(
+                              hintText: "Message ...",
+                              hintStyle: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 16,
+                              ),
+                              border: InputBorder.none
+                          ),
+                        )),
+                    SizedBox(width: 16,),
+                    GestureDetector(
+                      onTap: () {
+                        addMessage();
+                      },
+                      child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [
+                                    const Color(0x36FFFFFF),
+                                    const Color(0x0FFFFFFF)
+                                  ],
+                                  begin: FractionalOffset.topLeft,
+                                  end: FractionalOffset.bottomRight
+                              ),
+                              borderRadius: BorderRadius.circular(40)
+                          ),
+                          padding: EdgeInsets.all(12),
+                          child: Image.asset("assets/images/send.png",
+                            height: 25, width: 25,)),
+                    ),
+                  ],
                 ),
               ),
             ),
-            _buildMessageComposer(),
           ],
         ),
+      ),
+    );
+  }
+
+}
+
+class MessageTile extends StatelessWidget {
+  final String message;
+  final bool sendByMe;
+
+  MessageTile({required this.message, required this.sendByMe});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          top: 8,
+          bottom: 8,
+          left: sendByMe ? 0 : 24,
+          right: sendByMe ? 24 : 0),
+      alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: sendByMe
+            ? EdgeInsets.only(left: 30)
+            : EdgeInsets.only(right: 30),
+        padding: EdgeInsets.only(
+            top: 17, bottom: 17, left: 20, right: 20),
+        decoration: BoxDecoration(
+            borderRadius: sendByMe ? BorderRadius.only(
+                topLeft: Radius.circular(23),
+                topRight: Radius.circular(23),
+                bottomLeft: Radius.circular(23)
+            ) :
+            BorderRadius.only(
+                topLeft: Radius.circular(23),
+                topRight: Radius.circular(23),
+                bottomRight: Radius.circular(23)),
+            gradient: LinearGradient(
+              colors: sendByMe ? [
+                const Color(0xff007EF4),
+                const Color(0xff2A75BC)
+              ]
+                  : [
+                const Color(0x1AFFFFFF),
+                const Color(0x1AFFFFFF)
+              ],
+            )
+        ),
+        child: Text(message,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontFamily: 'OverpassRegular',
+                fontWeight: FontWeight.w300)),
       ),
     );
   }
